6e01348412b159662ff4b1e10a2b0296f49c788d
Hendrik Maus <aidentailor@gmail.com>
2022-03-02 17:20:10 +0100

    chore: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rust-pr-based-release-workflow
+
+an experiment for creating a pull-request based release workflow.
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("This tool does nothing; it is simply a placeholder.");
+    println!("I am experimenting with a pull-request based release workflow here")
+}
