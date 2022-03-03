938249e1dfc52e72339c493822d797342cb4a576
Hendrik Maus <aidentailor@gmail.com>
2022-03-03 21:09:36 +0100

    feat: implement some more logic
    - better logging
      - detailed tracing
      - bare minimum info
    
    - find pull-request by commit sha
      - get its labels
      - detect the magic label
        - defined as a constant for now
    
    - set gh-actions output depending
      on the pull-request's labels

--- release-rs/src/main.rs
+++ release-rs/src/main.rs
@@ -1,9 +1,48 @@
 use anyhow::Context;
+use cmd_lib::run_fun;
+use serde::Deserialize;
+
+// Name of the GitHub Actions output that following steps can read
+static OUTPUT_RELEASE_DETECTED: &str = "release-detected";
+
+// The name of the label to expect on release pull-requests
+static PULL_REQUEST_LABEL: &str = "autorelease";
+
+#[derive(Deserialize, Debug, Clone)]
+struct PullRequest {
+    number: u32,
+}
+
+#[derive(Deserialize, Debug)]
+struct Labels {
+    labels: Vec<Label>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Label {
+    name: String,
+}
+
+struct Release;
+
+impl Release {
+    #[allow(dead_code)]
+    fn hit() {
+        set_output(OUTPUT_RELEASE_DETECTED, "true")
+    }
+
+    fn miss() {
+        set_output(OUTPUT_RELEASE_DETECTED, "false")
+    }
+}
+
+// Set an "output" in GitHub Actions
+fn set_output(key: &str, value: &str) {
+    println!("::set-output name={key}::{value}");
+}
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
-
-    let mut release_detected = false;
 
     let start = std::time::Instant::now();
     log::trace!("starting release-rs process");
@@ -13,22 +52,80 @@ fn main() -> anyhow::Result<()> {
     log::trace!("got commit sha from GITHUB_SHA: {sha}");
 
     let commit: String = cmd_lib::run_fun!(git log --oneline -n 1 "${sha}")?;
-    log::info!("got commit: '{commit}'");
+    log::info!("found possible release commit:");
+    log::info!("\t{commit}");
 
+    // assert, and exit early, if the current commit message does not carry the word 'release'
+    // all release pull-requests will contain the word in their merge commit; however any commit
+    // could and therefore this is just a very rough indicator.
     if !commit.contains("release") {
-        log::debug!("commit doesn't contain the word 'release'");
-    } else {
-        // todo set release detected for now without further investigation
-        release_detected = true;
+        log::info!("commit doesn't contain the word 'release'");
+        Release::miss();
+        return Ok(());
     }
 
-    // todo use `gh` to get the pull-request (if any) of the current commit
-    // gh pr list --state merged --search ${sha} --limit 1 --json number,url,id,title
-    // the command returns an array of json object with the respective keys
+    // try and find the pull-request that the commit was part of to examine it
+    // a release can only ever be triggered by a pull-request being merged
+    let pr = find_pull_request_by(&sha)?;
 
-    println!("::set-output name=release-detected::{release_detected}");
+    match pr {
+        Some(_p) => {
+            log::info!("detected release");
+            // todo now we'd need to do additional parsing, e.g. get the tag to create
+            Release::hit();
+        }
+        None => {
+            log::info!("commit could not be found in a release pull-request");
+            Release::miss();
+        }
+    }
 
     log::trace!("finished release-rs process, took {:?}", start.elapsed());
 
     Ok(())
+}
+
+// Shells out to GitHub's CLI `gh` to try and determine if the commit belongs to any pull-request
+fn find_pull_request_by(sha: &str) -> anyhow::Result<Option<PullRequest>> {
+    log::trace!("listing pull-requests that contain the commit:");
+    let pulls = run_fun!(gh pr list --state merged --search ${sha} --limit 1 --json number)?;
+    log::trace!("{pulls}");
+
+    let pulls: Vec<PullRequest> = serde_json::from_str(&pulls)?;
+
+    if pulls.is_empty() {
+        log::trace!("the commit sha {sha} is not part of any pull-request");
+        return Ok(None);
+    }
+
+    if pulls.len() != 1 {
+        anyhow::bail!("the commit is part of more than pull-request; cannot parse at this time");
+    }
+
+    let pr = pulls.first().unwrap().clone();
+    log::trace!("extracted {pr:?}");
+
+    log::trace!("getting labels for the possibly qualified pull-request:");
+    let labels = {
+        let pr_number = pr.number;
+        run_fun!(gh pr view ${pr_number} --json labels)?
+    };
+    log::trace!("{labels}");
+
+    let labels: Labels = serde_json::from_str(&labels)?;
+
+    if labels.labels.is_empty() {
+        log::trace!(
+            "the commit sha {sha} is not part of any pull-request with the {PULL_REQUEST_LABEL} label"
+        );
+        return Ok(None);
+    }
+
+    for label in labels.labels {
+        if label.name == PULL_REQUEST_LABEL {
+            return Ok(Some(pr));
+        }
+    }
+
+    Ok(None)
 }
