a30a1212e2c2e761dd9bf77e71daa7d8556aeeb0
Hendrik Maus <aidentailor@gmail.com>
2022-03-03 09:05:46 +0100

    ci: move the logic from shell to rust

--- release-rs/src/main.rs
+++ release-rs/src/main.rs
@@ -1,4 +1,31 @@
-fn main() {
+use anyhow::Context;
+
+fn main() -> anyhow::Result<()> {
     env_logger::init();
-    log::info!("hello world")
+
+    let release_detected = false;
+
+    let start = std::time::Instant::now();
+    log::trace!("starting release-rs process");
+
+    let sha = std::env::var("GITHUB_SHA")
+        .context("Could not fetch GITHUB_SHA from the environment, is it set?")?;
+    log::trace!("got commit sha from GITHUB_SHA: {sha}");
+
+    let commit: String = cmd_lib::run_fun!(git log --oneline -n 1 "${sha}")?;
+    log::info!("got commit: '{commit}'");
+
+    if !commit.contains("release") {
+        log::debug!("commit doesn't contain the word 'release'");
+    }
+
+    // todo use `gh` to get the pull-request (if any) of the current commit
+    // gh pr list --state merged --search ${sha} --limit 1 --json number,url,id,title
+    // the command returns an array of json object with the respective keys
+
+    println!("::set-output name=release-detected::{release_detected}");
+
+    log::trace!("finished release-rs process, took {:?}", start.elapsed());
+
+    Ok(())
 }
