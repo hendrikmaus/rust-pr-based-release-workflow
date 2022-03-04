00e8ccc2a7e25ad26c09d7957db0d7ac63841773
Hendrik Maus <aidentailor@gmail.com>
2022-03-04 07:41:27 +0100

    refactor: move github code into its own module

--- release-rs/src/main.rs
+++ release-rs/src/main.rs
@@ -1,44 +1,26 @@
+mod github;
+
+use crate::github::{Actions, GitHub};
 use anyhow::Context;
-use cmd_lib::run_fun;
-use serde::Deserialize;
 
 // Name of the GitHub Actions output that following steps can read
+// todo make command line option with default value
 static OUTPUT_RELEASE_DETECTED: &str = "release-detected";
 
 // The name of the label to expect on release pull-requests
+// todo make command line option with default value
 static PULL_REQUEST_LABEL: &str = "autorelease";
-
-#[derive(Deserialize, Debug, Clone)]
-struct PullRequest {
-    number: u32,
-}
-
-#[derive(Deserialize, Debug)]
-struct Labels {
-    labels: Vec<Label>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Label {
-    name: String,
-}
 
 struct Release;
 
 impl Release {
-    #[allow(dead_code)]
     fn hit() {
-        set_output(OUTPUT_RELEASE_DETECTED, "true")
+        Actions::set_output(OUTPUT_RELEASE_DETECTED, "true")
     }
 
     fn miss() {
-        set_output(OUTPUT_RELEASE_DETECTED, "false")
+        Actions::set_output(OUTPUT_RELEASE_DETECTED, "false")
     }
-}
-
-// Set an "output" in GitHub Actions
-fn set_output(key: &str, value: &str) {
-    println!("::set-output name={key}::{value}");
 }
 
 fn main() -> anyhow::Result<()> {
@@ -66,7 +48,7 @@ fn main() -> anyhow::Result<()> {
 
     // try and find the pull-request that the commit was part of to examine it
     // a release can only ever be triggered by a pull-request being merged
-    let pr = find_pull_request_by(&sha)?;
+    let pr = GitHub::find_pull_request_by(&sha, PULL_REQUEST_LABEL)?;
 
     match pr {
         Some(_p) => {
@@ -84,49 +66,4 @@ fn main() -> anyhow::Result<()> {
     log::trace!("finished release-rs process, took {:?}", start.elapsed());
 
     Ok(())
-}
-
-// Shells out to GitHub's CLI `gh` to try and determine if the commit belongs to any pull-request
-fn find_pull_request_by(sha: &str) -> anyhow::Result<Option<PullRequest>> {
-    log::trace!("listing pull-requests that contain the commit:");
-    let pulls = run_fun!(gh pr list --state merged --search ${sha} --limit 1 --json number)?;
-    log::trace!("{pulls}");
-
-    let pulls: Vec<PullRequest> = serde_json::from_str(&pulls)?;
-
-    if pulls.is_empty() {
-        log::trace!("the commit sha {sha} is not part of any pull-request");
-        return Ok(None);
-    }
-
-    if pulls.len() != 1 {
-        anyhow::bail!("the commit is part of more than pull-request; cannot parse at this time");
-    }
-
-    let pr = pulls.first().unwrap().clone();
-    log::trace!("extracted {pr:?}");
-
-    log::trace!("getting labels for the possibly qualified pull-request:");
-    let labels = {
-        let pr_number = pr.number;
-        run_fun!(gh pr view ${pr_number} --json labels)?
-    };
-    log::trace!("{labels}");
-
-    let labels: Labels = serde_json::from_str(&labels)?;
-
-    if labels.labels.is_empty() {
-        log::trace!(
-            "the commit sha {sha} is not part of any pull-request with the {PULL_REQUEST_LABEL} label"
-        );
-        return Ok(None);
-    }
-
-    for label in labels.labels {
-        if label.name == PULL_REQUEST_LABEL {
-            return Ok(Some(pr));
-        }
-    }
-
-    Ok(None)
 }
